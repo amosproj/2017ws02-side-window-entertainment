3aec746b94c2c4e2a9b79b6819c760406c01dfc7
Ursegol <J.Schulte3@gmx.de>
2017-11-08 21:54:27 +0100

    got rid of the Gson requirement. implemented two input possibilities (parameter or command line). Included Hai's getInfoBox function, which requires a parser to be fully usable.

--- src/tub.swit.information_source/WikiAPI.java
+++ src/tub.swit.information_source/WikiAPI.java
@@ -1,44 +1,101 @@
+package tub.swit;
+
 import java.io.*;
 import java.net.*;
+import java.util.Scanner;
 
 public class WikiAPI {
 
     public static String language;
-    public static Gson gson;
 
     public static void main(String[] args) {
         language = "en";
-        gson = new Gson();
-        ResultQueryTitles rqt = searchArticles("HTML");
-        if(rqt.query.pages.ids.isEmpty()) {
+        String searchWord = "";
+        if(args.length == 1) {
+        	searchWord = args[0];
+        } else {
+        	System.out.println("Please enter the word of which you want the information of to be retrieved!");
+        	Scanner scanner = new Scanner(System.in);
+        	searchWord = scanner.nextLine();
+        	scanner.close();
+		}
+        
+        searchWord = searchWord.replaceAll(" ", "_");
+        
+        int rqt = searchArticles(searchWord);
+        if(rqt == -1) {
             System.out.println("no pages found");
         } else {
-            System.out.println(getExtract(rqt.query.pages.ids.get(0)));
+            System.out.println(getExtract(rqt));
         }
     }
-
-    public static ResultQueryTitles searchArticles(String searchTerm) {
+    
+    /*
+     * Getter function to retrieve the article information.
+     */
+    public String getArticle(String articleName) {
+		String article = "";
+		int articleID = searchArticles(articleName);
+    	article = getExtract(articleID);
+    	return article;
+	}
+    
+    /*
+     * Searches for the Wikipedia article ID and returns -1 if no page was found.
+     */
+    private static int searchArticles(String searchTerm) {
+    	String json = "";
+    	int result = -1;
         try {
-            String json = readHTTP("https://" + language + ".wikipedia.org/w/api.php?action=query&format=json&titles=" + searchTerm);
+            json = readHTTP("https://" + language + ".wikipedia.org/w/api.php?action=query&format=json&titles=" + searchTerm);
+            int idStartIndex = json.indexOf("pages\":{\"") + 9;
+            json = json.substring(idStartIndex);
+            int idEndIndex = json.indexOf("\"");
+            json = json.substring(0, idEndIndex);
+            result = Integer.parseInt(json);
         } catch(Exception e) {
             e.printStackTrace();
         }
-        ResultQueryTitles result = gson.fromJson(json, ResultQueryTitles.class);
         return result;
     }
 
-    public static String getExtract(int atricleID) {
+    /*
+     * Retrieves the short information text for a Wikipedia article with the given ID and returns an empty string if the ID is invalid.
+     */
+    private static String getExtract(int articleID) {
         String result = "";
         try {
-            String json = readHTTP("https://" + language + ".wikipedia.org/w/api.php?action=query&prop=extracts&exintro&explaintext&format=json&pageids=" + articleID);            
+            String json = readHTTP("https://" + language + ".wikipedia.org/w/api.php?action=query&prop=extracts&exintro&explaintext&format=json&pageids=" + articleID);
+            int idStartIndex = json.indexOf("extract\":\"") + 10;
+            result = json.substring(idStartIndex);
+            int idEndIndex = result.indexOf("\"");
+            result = result.substring(0, idEndIndex);
         } catch(Exception e) {
             e.printStackTrace();
         }
-        ResultQueryTitles result = gson.fromJson(json, ResultQueryTitles.class);
-        return result.query.pages.ids.get(0).extract;
+        return result;
     }
 
-    public static String readHTTP(String websiteURL) throws Exception {
+    /*
+     * Retrieves the infobox content of a Wikipedia article with the given search term. 
+     * TODO: requires a complex parser (maybe DBPedia is better?)
+     */
+    private static String getInfoBox(String searchTerm) {
+    	String json = "";
+    	try {
+    		json = readHTTP("https://guarded-ridge-13729.herokuapp.com/" + searchTerm);
+    	} catch (Exception e) {
+			e.printStackTrace();
+		}
+    	System.out.println(json);
+		return json;
+    }
+    
+    
+    /*
+     * Reads the HTTP informations of a given URL and returns it as a string. 
+     */
+    private static String readHTTP(String websiteURL) throws Exception {
         StringBuilder result = new StringBuilder();
         URL url = new URL(websiteURL);
         HttpURLConnection connection = (HttpURLConnection) url.openConnection();
