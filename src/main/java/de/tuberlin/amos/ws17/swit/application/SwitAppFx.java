f39555dc4810c7b229432ccb21cd98413b6b458d
Christian Fengler <christian.fengler@campus.tu-berlin.de>
2017-11-09 22:12:31 +0100

    Add SwitApp

---
+++ src/main/java/de/tuberlin/amos/ws17/swit/application/SwitAppFx.java
@@ -0,0 +1,16 @@
+package de.tuberlin.amos.ws17.swit.application;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class SwitAppFx extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+}
