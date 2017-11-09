4b89b2933b6ddb9d0fddce1bc203513a6b403741
Christian Fengler <christian.fengler@campus.tu-berlin.de>
2017-11-09 21:27:17 +0100

    Add SwitApp

---
+++ src/main/java/de/tuberlin/amos/ws17/swit/application/SwitApp.java
@@ -0,0 +1,11 @@
+package de.tuberlin.amos.ws17.swit.application;
+
+import java.io.Console;
+
+public class SwitApp {
+
+    public static void main(String[] args) {
+        System.out.println("Hello Swit-Team");
+    }
+
+}
