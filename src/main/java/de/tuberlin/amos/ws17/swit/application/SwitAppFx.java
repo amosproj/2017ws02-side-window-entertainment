d08b4785267cd34a4dc115d752f1f3d99dee6f8b
Chinh Tran <chinh.tran@mail.de>
2017-11-09 22:15:07 +0100

    Add test javafx window

--- src/main/java/de/tuberlin/amos/ws17/swit/application/SwitAppFx.java
+++ src/main/java/de/tuberlin/amos/ws17/swit/application/SwitAppFx.java
@@ -1,6 +1,11 @@
 package de.tuberlin.amos.ws17.swit.application;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 public class SwitAppFx extends Application {
@@ -11,6 +16,20 @@ public class SwitAppFx extends Application {
 
     @Override
     public void start(Stage primaryStage) {
+        primaryStage.setTitle("Hello World!");
+        Button btn = new Button();
+        btn.setText("Say 'Hello World'");
+        btn.setOnAction(new EventHandler<ActionEvent>() {
 
+            @Override
+            public void handle(ActionEvent event) {
+                System.out.println("Hello World!");
+            }
+        });
+
+        StackPane root = new StackPane();
+        root.getChildren().add(btn);
+        primaryStage.setScene(new Scene(root, 300, 250));
+        primaryStage.show();
     }
 }
