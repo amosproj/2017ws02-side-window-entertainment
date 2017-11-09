d3ee3d11f471ea13fe75e3c4f4e327dd50f8c906
Chinh Tran <chinh.tran@mail.de>
2017-11-09 21:21:00 +0100

    Add GPS classes

---
+++ src/main/java/de/tuberlin/amos/ws17/swit/gps/GpsTracker.java
@@ -0,0 +1,5 @@
+package de.tuberlin.amos.ws17.swit.gps;
+
+public interface GpsTracker {
+	GpsPosition GetGpsPosition();
+}
---
+++ src/main/java/de/tuberlin/amos/ws17/swit/gps/GpsTrackerFactory.java
@@ -0,0 +1,14 @@
+package de.tuberlin.amos.ws17.swit.gps;
+
+public class GpsTrackerFactory {
+	
+	private static GpsTracker gpsTracker = null;
+	
+	public static GpsTracker GetGpsTracker() {
+		if (gpsTracker == null) {
+			gpsTracker = new GpsTrackerImplementation();
+		}
+		
+		return gpsTracker;
+	}
+}
