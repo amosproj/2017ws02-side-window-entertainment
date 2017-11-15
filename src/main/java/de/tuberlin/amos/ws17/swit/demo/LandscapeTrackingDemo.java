4d5baafc25af6b5a01540abc9228f8687382fe91
Julian Legler <julian.legler@gmx.de>
2017-11-15 22:34:41 +0100

    made webcam capturing more fail proof

--- src/main/java/de/tuberlin/amos/ws17/swit/demo/LandscapeTrackingDemo.java
+++ src/main/java/de/tuberlin/amos/ws17/swit/demo/LandscapeTrackingDemo.java
@@ -22,12 +22,14 @@ public class LandscapeTrackingDemo {
 
             // fake stream
             img = lt.getImage();
-            while (frame.isVisible() && img != null) {
-                frame.showImage(img);
+            do {
+                if(img != null) {
+                    frame.showImage(img);
+                }
                 img = lt.getImage();
                 Thread.sleep(100);
-            }
-            frame.dispose();
+            } while (frame.isVisible() && img == null);
+            //frame.dispose();
             lt.webcam.stop();
         } catch (Exception e) {
             e.printStackTrace();
