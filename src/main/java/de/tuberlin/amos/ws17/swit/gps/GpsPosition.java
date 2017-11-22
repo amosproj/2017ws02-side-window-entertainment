5366a0c0eb932654a13f350d66abcd271eef6e59
etho2183 <vladimir.alekseychuk@hotmail.de>
2017-11-22 22:56:39 +0100

    Update GpsPosition.java

--- src/main/java/de/tuberlin/amos/ws17/swit/gps/GpsPosition.java
+++ src/main/java/de/tuberlin/amos/ws17/swit/gps/GpsPosition.java
@@ -5,34 +5,73 @@ public class GpsPosition {
 		this.timeStamp = timeStamp;
 		this.latitude = latitude;
 		this.longitude = longitude;
+		speedUpdate = false;
+		courseUpdate = false;
 	}
 
-	/**
-	 * Milliseconds from January 1, 1970
-	 */
+	// milliseconds from January 1, 1970
 	private long timeStamp;
 
-	/**
-	 * Latitude
-	 */
+	// latitude
 	private double latitude;
 
-	/**
-	 * Longitude
-	 */
+	// longitude
 	private double longitude;
 
+<<<<<<< HEAD
+	// course in degrees
+	private double course;
+	private boolean courseUpdate;
+
+	// speed in km/h
+	private double speed;
+	private boolean speedUpdate;
+
+	public double getTimeStamp() {
+=======
 	public long getTimeStamp() {
+>>>>>>> 0041ac2a01e0570b1db3d4cf19bd3c9339f939be
 		return timeStamp;
+	}
+	public void setTimeStamp(long t) {
+		this.timeStamp = t;
 	}
 
 	public double getLatitude() {
 		return latitude;
 	}
+	public void setLatitude(double lat) {
+		this.latitude = lat;
+	}
 
 	public double getLongitude() {
 		return longitude;
 	}
+	public void setLongitude(double lon) {
+		this.longitude = lon;
+	}
+
+	public double getCourse() {
+		courseUpdate = false;
+		return course;
+	}
+	public void setCourse(double course) {
+		courseUpdate = true;
+		this.course = course;
+	}
+
+	public boolean getCourseUpdate(){ return courseUpdate; }
+
+	public double getSpeed() {
+		speedUpdate = false;
+		return speed;
+	}
+	public void setSpeed(double speed) {
+		speedUpdate = true;
+		this.speed = speed;
+	}
+
+	public boolean getSpeedUpdate(){ return speedUpdate; }
 
 	@Override
 	public String toString() {
