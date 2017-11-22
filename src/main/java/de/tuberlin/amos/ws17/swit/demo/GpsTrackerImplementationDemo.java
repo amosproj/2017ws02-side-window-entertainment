2cf1506cfefe74d6f375c910521b85491bedcd4f
etho2183 <vladimir.alekseychuk@hotmail.de>
2017-11-22 23:04:52 +0100

    Update GpsTrackerImplementationDemo.java

--- src/main/java/de/tuberlin/amos/ws17/swit/demo/GpsTrackerImplementationDemo.java
+++ src/main/java/de/tuberlin/amos/ws17/swit/demo/GpsTrackerImplementationDemo.java
@@ -3,14 +3,43 @@ package de.tuberlin.amos.ws17.swit.demo;
 import de.tuberlin.amos.ws17.swit.gps.GpsPosition;
 import de.tuberlin.amos.ws17.swit.gps.GpsTrackerImplementation;
 
+import java.util.LinkedList;
+
 public class GpsTrackerImplementationDemo {
 
     public static void main(String[] args){
-        GpsTrackerImplementation tracker = new GpsTrackerImplementation("C:\\file.txt");
-        GpsPosition pos = tracker.getGpsPosition();
-        if(pos != null)
-            System.out.println("Latest position: " + pos.getLatitude() + ", " + pos.getLongitude() + " at " + pos.getTimeStamp());
-        else
-            System.out.println("Latest position: null");
+        GpsTrackerImplementation tracker = new GpsTrackerImplementation();
+        try{
+            int i = 0;
+            while(i < 30){
+                i++;
+                Thread.sleep(1000);
+                GpsPosition pos = tracker.getGpsPosition();
+                if(pos != null)
+                    System.out.println("Latest position: " + pos.getLatitude() + ", " + pos.getLongitude()
+                            + " at " + pos.getTimeStamp()+ " with speed: " + pos.getSpeed() + " and course : " + pos.getCourse());
+                else
+                    System.out.println("Latest position: null");
+            }
+
+            LinkedList<GpsPosition> gpsList = tracker.getGpsList();
+            if(gpsList != null){
+                System.out.println("gpsList start:");
+                while(gpsList.size() > 0){
+                    GpsPosition element = gpsList.pop();
+                    System.out.println("Latest position: " + element.getLatitude() + ", " + element.getLongitude() + " at "
+                            + element.getTimeStamp() + " with speed: " + element.getSpeed() + " and course: " + element.getCourse());
+                }
+                System.out.println("gpsList end");
+            }
+            else{
+                System.out.println("gpsList is empty");
+            }
+
+        }
+        catch(java.lang.InterruptedException e){
+            System.out.println("InterruptedEx exception");
+        }
     }
+
 }
