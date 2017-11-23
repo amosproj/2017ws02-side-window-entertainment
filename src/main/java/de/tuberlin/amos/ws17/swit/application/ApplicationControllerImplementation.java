9a84eef991652f6e2732456caa0250c4d5a40423
Christian Fengler <christian.fengler@campus.tu-berlin.de>
2017-11-23 20:31:04 +0100

    propertychanged für ApplicationController

--- src/main/java/de/tuberlin/amos/ws17/swit/application/ApplicationControllerImplementation.java
+++ src/main/java/de/tuberlin/amos/ws17/swit/application/ApplicationControllerImplementation.java
@@ -3,14 +3,31 @@ package de.tuberlin.amos.ws17.swit.application;
 import de.tuberlin.amos.ws17.swit.common.PointOfInterest;
 
 import java.awt.image.BufferedImage;
+import java.beans.PropertyChangeListener;
+import java.beans.PropertyChangeSupport;
 import java.util.List;
 
 public class ApplicationControllerImplementation implements ApplicationController {
+
+    private PropertyChangeSupport propertyChangeSupport = new PropertyChangeSupport(this);
 
     private List<PoiViewModel> GpsPois;
     private List<PoiViewModel> LandscapePois;
 
     private List<PointOfInterest> pointsOfInterest;
+
+    private String title;
+
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        String oldTitle = this.title;
+        this.title = title;
+        propertyChangeSupport.firePropertyChange("title", oldTitle, title);
+    }
 
     public static void main(String[] args) {
 
@@ -19,4 +36,13 @@ public class ApplicationControllerImplementation implements ApplicationControlle
     public void addPOI(int id, String name, BufferedImage image, String information) {
 
     }
+
+    public void addPropertyChangeListener(PropertyChangeListener listener) {
+        propertyChangeSupport.addPropertyChangeListener(listener);
+    }
+
+    public void removePropertyChangeListener(PropertyChangeListener listener) {
+        propertyChangeSupport.removePropertyChangeListener(listener);
+    }
+
 }
