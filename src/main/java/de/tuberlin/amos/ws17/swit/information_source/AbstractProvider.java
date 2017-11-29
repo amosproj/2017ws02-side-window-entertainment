6e83c3ecbd4dab8ee2953ebf7a31ec3abcf06d92
Ursegol <J.Schulte3@gmx.de>
2017-11-29 20:26:14 +0100

    created an interface, which provides a function to provide an information abstract for a poi object

---
+++ src/main/java/de/tuberlin/amos/ws17/swit/information_source/AbstractProvider.java
@@ -0,0 +1,14 @@
+package de.tuberlin.amos.ws17.swit.information_source;
+
+import de.tuberlin.amos.ws17.swit.common.PointOfInterest;
+
+public interface AbstractProvider {
+
+    /**
+     * Provides an abstract for a POI
+     * @param poi PointOfInterest object, already contains either a name of a poi or its poi
+     */
+    void provideAbstract(PointOfInterest poi);
+
+
+}
