0ca686dee9d151bb367d1264ea7f089bb866835c
loriander <kruse-ziegler@campus.tu-berlin.de>
2017-11-29 18:45:28 +0100

    Edited equals and Hashcode fot PointOfInterest.java to only compare according to ID. Added a Constructor which requires the minimum Requirements of a POI.

--- src/main/java/de/tuberlin/amos/ws17/swit/common/PointOfInterest.java
+++ src/main/java/de/tuberlin/amos/ws17/swit/common/PointOfInterest.java
@@ -5,11 +5,20 @@ import javafx.scene.image.Image;
 import java.awt.image.BufferedImage;
 
 public class PointOfInterest {
-    private String id; //TODO: ist id als String auszudrücken?
+
+    private String id;
     private String name;
     private GpsPosition gpsPosition;
     private BufferedImage image;
     private String informationAbstract;
+
+    public PointOfInterest() {
+    }
+    public PointOfInterest(String id, String name, GpsPosition gpsPosition) {
+        this.id = id;
+        this.name = name;
+        this.gpsPosition = gpsPosition;
+    }
 
     public String getId() {
         return id;
@@ -58,10 +67,12 @@ public class PointOfInterest {
 
         PointOfInterest that = (PointOfInterest) o;
 
-        if (id != null ? !id.equals(that.id) : that.id != null) return false;
-        if (name != null ? !name.equals(that.name) : that.name != null) return false;
-        if (gpsPosition != null ? !gpsPosition.equals(that.gpsPosition) : that.gpsPosition != null) return false;
-        if (image != null ? !image.equals(that.image) : that.image != null) return false;
-        return informationAbstract != null ? informationAbstract.equals(that.informationAbstract) : that.informationAbstract == null;
+        return id.equals(that.id);
     }
+
+    @Override
+    public int hashCode() {
+        return id.hashCode();
+    }
+
 }
