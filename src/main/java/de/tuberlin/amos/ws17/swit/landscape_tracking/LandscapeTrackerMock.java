27ce916080cc7dcac4544e3eccb9a274c652b3d9
CFengler <christian.fengler@yahoo.de>
2017-12-19 19:40:10 +0100

    überarbeitung der Mock Klasse so dass nicht immer ein neuen random image zurückgegebn wird sondern nur alle 6 Sekunden

--- src/main/java/de/tuberlin/amos/ws17/swit/landscape_tracking/LandscapeTrackerMock.java
+++ src/main/java/de/tuberlin/amos/ws17/swit/landscape_tracking/LandscapeTrackerMock.java
@@ -2,24 +2,41 @@ package de.tuberlin.amos.ws17.swit.landscape_tracking;
 
 import de.tuberlin.amos.ws17.swit.common.exceptions.ModuleNotWorkingException;
 import de.tuberlin.amos.ws17.swit.image_analysis.ImageUtils;
+import org.joda.time.DateTime;
+import org.joda.time.Seconds;
 
 import java.awt.image.BufferedImage;
 import java.io.IOException;
 
 public class LandscapeTrackerMock implements LandscapeTracker {
 
-    boolean isTracking = false;
+    private boolean isTracking = false;
+
+    private DateTime dateTimeLoadImage = null;
+    private BufferedImage currentImage = null;
 
     @Override
     public BufferedImage getImage() throws IOException {
-        if (isTracking)
-            return ImageUtils.getRandomTestImage();
-        return null;
+        if (!isTracking)
+            return null;
+
+        if (Seconds.secondsBetween(dateTimeLoadImage, new DateTime()).getSeconds() > 7) {
+            loadImage();
+        }
+
+        return currentImage;
     }
 
     @Override
     public void startModule() throws ModuleNotWorkingException {
         isTracking = true;
+
+        loadImage();
+    }
+
+    private void loadImage() {
+        currentImage = ImageUtils.getRandomTestImage();
+        dateTimeLoadImage = new DateTime();
     }
 
     @Override
