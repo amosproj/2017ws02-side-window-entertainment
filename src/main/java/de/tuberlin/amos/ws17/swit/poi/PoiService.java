b53f15cbc52d8e05a77807bba9ebd574105290c9
loriander <kruse-ziegler@campus.tu-berlin.de>
2017-12-21 01:38:58 +0100

    A little documentation added

--- src/main/java/de/tuberlin/amos/ws17/swit/poi/PoiService.java
+++ src/main/java/de/tuberlin/amos/ws17/swit/poi/PoiService.java
@@ -15,10 +15,29 @@ import java.util.Set;
  */
 public interface PoiService<T extends PointOfInterest> {
 
+    /**
+     * Recive all POIs for a circle
+     * @param center as the centerpoint
+     * @param radius as the radius in meters
+     * @return a List of the found POIs
+     * @throws InvalidRequestException for an invalid requests
+     */
     List<T> loadPlaceForCircle(GpsPosition center, int radius) throws InvalidRequestException;
 
+    /**
+     * Recive all POIs for a circle
+     * @param center as the centerpoint
+     * @param radius as the radius in meters
+     * @param types  as the types to retrieve
+     * @return a List of the found POIs
+     * @throws InvalidRequestException for an invalid requests
+     */
     List<T> loadPlaceForCircleAndPoiType(GpsPosition center, int radius, PoiType... types) throws InvalidRequestException;
 
+    /**
+     * Loding photos expensive so it's done separately
+     * @param poisToAddPhotosTo as the POIs where a try of adding a photo is made
+     */
     void addImages(Collection<T> poisToAddPhotosTo);
 
 }
