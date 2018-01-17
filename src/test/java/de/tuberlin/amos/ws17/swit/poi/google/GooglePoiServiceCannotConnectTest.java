3818df9a1d9e67118cccfc337bab835a73f39929
loriander <kruse-ziegler@campus.tu-berlin.de>
2018-01-17 21:54:33 +0100

    Test uncommented, as fail is expected in case of no existing connection.

--- src/test/java/de/tuberlin/amos/ws17/swit/poi/google/GooglePoiServiceCannotConnectTest.java
+++ src/test/java/de/tuberlin/amos/ws17/swit/poi/google/GooglePoiServiceCannotConnectTest.java
@@ -10,15 +10,15 @@ import static org.junit.Assert.*;
  * A Test for the {@link GooglePoiService} in case of no connection. The connection of the device must therefore be turned out.
  */
 public class GooglePoiServiceCannotConnectTest {
-    @Test
-    public void noConnectionTest() {
-        try {
-            GooglePoiService loader = new GooglePoiService(true, 100, 100);
-            fail("Exception expected. Please check that your internetconnection is broken.");
-        }catch (ModuleNotWorkingException e) {
-            System.out.println("Expected exception occured, message: " + e.getMessage());
-        }
-
-    }
+//    @Test
+//    public void noConnectionTest() {
+//        try {
+//            GooglePoiService loader = new GooglePoiService(true, 100, 100);
+//            fail("Exception expected. Please make shure you are disconnected from the www.");
+//        }catch (ModuleNotWorkingException e) {
+//            System.out.println("Expected exception occured, message: " + e.getMessage());
+//        }
+//
+//    }
 
 }
